24572921838fb3bdf917c79ae323437dea406fde
Eberton <ebertonsouza@unigranrio.br>
2023-09-19 12:52:58 -0300

    starting portfolio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={ <h1>Home</h1> } />
+    </Routes>
+  );
+}
+
+export default App;
