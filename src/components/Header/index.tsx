ad768bac970323b68964579c70f2161a1d92ab9d
Eberton <ebertonsouza@unigranrio.br>
2023-09-19 13:51:15 -0300

    build: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>Página inicial</li>
+        <li>Sobre mim</li>
+        <li>Portfólio</li>
+        <li>Contato</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
