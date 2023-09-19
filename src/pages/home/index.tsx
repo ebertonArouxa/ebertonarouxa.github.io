c7777f52b53ae951e344ec27d4c31ebd938c57df
Eberton <ebertonsouza@unigranrio.br>
2023-09-19 13:51:30 -0300

    build: create home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h2>Hello world, meu nome é:</h2>
+        <h1>Eberton Arouxa</h1>
+        <p>resumo sobre mim</p>
+        <button type="button">Sobre mim</button>
+      </div>
+      <div>
+        <img src="" alt="foto de eberton arouxa" />
+      </div>
+    </>
+  );
+}
+
+export default Home;
