0b68c4e70c5c5bd0e5cf3fb9239a8ea771e66f95
Eberton <ebertonsouza@unigranrio.br>
2023-09-22 03:10:38 -0300

    style: change header to be fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,45 +2,47 @@ import styles from './styles.module.css';
 
 function Header() {
   return (
-    <nav className={ styles.headerWrapper }>
-      <div>
-        <span className={ styles.specialCharacter }>&lt;</span>
-        <span className={ styles.headerTitle }>
-          {'Hello World '}
-        </span>
-        <span className={ styles.specialCharacter }>/&gt;</span>
-      </div>
-      <ul className={ styles.headerNavWrapper }>
-        <li>
-          <span className={ styles.specialNavCharacter }>&lt;</span>
-          <span className={ styles.headerNav }>
-            {'Página inicial '}
+    <div className={ styles.fixedHeader }>
+      <nav className={ styles.headerWrapper }>
+        <div>
+          <span className={ styles.specialCharacter }>&lt;</span>
+          <span className={ styles.headerTitle }>
+            {'Hello World '}
           </span>
-          <span className={ styles.specialNavCharacter }>/&gt;</span>
-        </li>
-        <li>
-          <span className={ styles.specialNavCharacter }>&lt;</span>
-          <span className={ styles.headerNav }>
-            {'Sobre mim '}
-          </span>
-          <span className={ styles.specialNavCharacter }>/&gt;</span>
-        </li>
-        <li>
-          <span className={ styles.specialNavCharacter }>&lt;</span>
-          <span className={ styles.headerNav }>
-            {'Portfólio '}
-          </span>
-          <span className={ styles.specialNavCharacter }>/&gt;</span>
-        </li>
-        <li>
-          <span className={ styles.specialNavCharacter }>&lt;</span>
-          <span className={ styles.headerNav }>
-            {'Contato '}
-          </span>
-          <span className={ styles.specialNavCharacter }>/&gt;</span>
-        </li>
-      </ul>
-    </nav>
+          <span className={ styles.specialCharacter }>/&gt;</span>
+        </div>
+        <ul className={ styles.headerNavWrapper }>
+          <li>
+            <span className={ styles.specialNavCharacter }>&lt;</span>
+            <span className={ styles.headerNav }>
+              {'Página inicial '}
+            </span>
+            <span className={ styles.specialNavCharacter }>/&gt;</span>
+          </li>
+          <li>
+            <span className={ styles.specialNavCharacter }>&lt;</span>
+            <span className={ styles.headerNav }>
+              {'Sobre mim '}
+            </span>
+            <span className={ styles.specialNavCharacter }>/&gt;</span>
+          </li>
+          <li>
+            <span className={ styles.specialNavCharacter }>&lt;</span>
+            <span className={ styles.headerNav }>
+              {'Portfólio '}
+            </span>
+            <span className={ styles.specialNavCharacter }>/&gt;</span>
+          </li>
+          <li>
+            <span className={ styles.specialNavCharacter }>&lt;</span>
+            <span className={ styles.headerNav }>
+              {'Contato '}
+            </span>
+            <span className={ styles.specialNavCharacter }>/&gt;</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
